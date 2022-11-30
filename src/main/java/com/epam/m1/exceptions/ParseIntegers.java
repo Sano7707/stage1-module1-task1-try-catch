ad54d39821c7e173f6f07944e179dc5cb63791f4
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-30 23:20:36 +0400

    Changes

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -18,10 +18,17 @@ public class ParseIntegers {
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
-        String justWords = "";
+        StringBuilder justWords = new StringBuilder();
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
+            int number = 0;
+            try {
+                number = Integer.parseInt(next);
+                sum+=  number;
+            }
+            catch (NumberFormatException e){
+               justWords.append(" ").append(String.join(" ", next));
+            }
             // todo: complete it
         }
         System.out.println("Sum is " + sum);
